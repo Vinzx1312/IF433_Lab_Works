b720ad1dbb3921f2f868b259a91949b665c5d9ee
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-03 18:51:26 +0700

    week01: initial setup and grading system

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week01
+
+fun main(){
+    var name: String = "Jogn Thor"
+    var score: Int = 80
+
+    //concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
