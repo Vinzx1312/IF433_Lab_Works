0e0e157da092a226b42b135fdfd009e6044161a0
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-03 19:31:05 +0700

    week01: (tugas 1) clean up unused argos and comments Array<String>

--- src/oop_00000131636_KevinIvanSetiadiPhang/week01/CircleCalculator.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week01/CircleCalculator.kt
@@ -4,17 +4,21 @@ fun main(args: Array<String>) {
     // Variable definition
     val radius = 7.0
     val pi = 3.14
+
     // Calculation
     val area = pi * radius * radius
-// Output Concatenation
+
+    // Output Concatenation
     println("Radius: $radius , Area: $area")
+
     // Logic check
-    checkSize(area)
+    println(checkSize(area))
 }
-fun checkSize(area: Double) {
+
+// Expression Body Function
+fun checkSize(area: Double) =
     if (area > 100) {
-        println("This is a Big Circle")
+        "This is a Big Circle"
     } else {
-        println("This is a Small Circle")
+        "This is a Small Circle"
     }
-}
