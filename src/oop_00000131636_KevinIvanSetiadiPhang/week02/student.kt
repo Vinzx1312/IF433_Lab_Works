6c1eee6cc543951ccb6207c8b8d27f1e66b82c8b
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-10 15:51:11 +0700

    week02: add secondary constructor

--- src/oop_00000131636_KevinIvanSetiadiPhang/week02/student.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week02/student.kt
@@ -5,13 +5,19 @@ class student(
     val nim:String,
     var major:String
 ){
-    init{
+    init {
         //validasi sederhana cek panjang NIM
-        if (nim.length != 5){
+        if (nim.length != 5) {
             println("WARNING :Objek tercipta  dengan NIM ($nim) yang tidak valid")
             println("Data  mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek student $name berhasil dialokasikan di Memory.")
+        }
+    }
+        //secondary Constructor
+        //wajib memanggil primary Constructor menggunakna "this"
+        constructor(name: String, nim: String) : this(name, nim, major="Non-Matriculated") {
+            println("LOG: Menggunakan constructor jalur umum ( Tanpa Jurusan).")
         }
     }
     //body class kosong dulu
