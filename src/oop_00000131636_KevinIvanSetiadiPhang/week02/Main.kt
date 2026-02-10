8c95bf24ea50f239d8eaccad3dd95ec2a72e631a
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-10 16:07:38 +0700

    week02: implement selection logic for constructors

--- src/oop_00000131636_KevinIvanSetiadiPhang/week02/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week02/Main.kt
@@ -28,4 +28,24 @@ fun main() {
         val s1 = Student(name, nim, major)
         println("Status: Pendaftaran Selesai.")
     }
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine() // consume newline
+
+    if (type == 1) {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // Primary Constructor
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+    } else if (type == 2) {
+        // Secondary Constructor
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+    } else {
+        println("Pilihan ngawur, pendaftaran batal!")
+    }
 }
