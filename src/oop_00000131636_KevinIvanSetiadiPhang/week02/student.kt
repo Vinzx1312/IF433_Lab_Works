4cd3297d977a9672e3ea336e182526653726d308
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-10 15:22:34 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week02/student.kt
@@ -0,0 +1,12 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week02
+
+class student(
+    val name:String,
+    val nim:String,
+    var major:String
+){
+    init{
+        //validasi sederhana cek panjang
+    }
+    //body class kosong dulu
+}
