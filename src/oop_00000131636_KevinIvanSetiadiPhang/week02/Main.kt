5525b7227324d07f42c0d68fa2718a28efd394ee
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-10 15:44:27 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week02/Main.kt
@@ -0,0 +1,31 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PM UMN ---")
+
+    print("Memasukkan Nama: ")
+    val name= scanner.nextLine()
+
+    print("Memasukkan NIM (Wajib 5 karakter):   ")
+    val nim= scanner.nextLine()
+
+    scanner.nextLine() // bersihkan buffer newline (penyakit klasik scanner)
+
+    //validasi di sisi pemanggil (main)
+
+    if(nim.length != 5) {
+        println("ERROR : Pendaftaran dibatalkan, NIM harus 5 karakter!")
+        //program berhenti disini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Memasukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        //onstalasi objek karena data sudah aman
+        val s1 = student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
