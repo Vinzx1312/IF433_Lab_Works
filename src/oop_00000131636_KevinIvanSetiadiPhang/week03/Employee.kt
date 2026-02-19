94e30420873659aa73377b8cd552660331434033
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-20 00:12:19 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000131636_KevinIvanSetiadiPhang/week03/Employee.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_00000131636_KevinIvanSetiadiPhang.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+    set(value) {
+        println("Mencoba set gaji ke: $value")
+        //Peringatan: kode di bawah ini salah dan berbahaya!
+        //Kita memanggil setter di dalam setter ( recursive)
+        this.salary = value
+    }
 }
---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // ini akan meledakkan  program anda
+}
