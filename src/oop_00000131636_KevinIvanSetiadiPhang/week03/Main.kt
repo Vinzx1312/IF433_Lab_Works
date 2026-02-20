8f1486896116cae43c0b798f3ac23942e94a6e5e
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-20 15:53:59 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000131636_KevinIvanSetiadiPhang/week03/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week03/Main.kt
@@ -2,6 +2,18 @@ package oop_00000131636_KevinIvanSetiadiPhang.week03
 
 fun main() {
     val e = Employee("Budi")
+
+    //1. test validasi salary
+    e.salary = -1000 // harusnya salary
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    //2. test encapsulation
+    e.increasePerformance()
+    //e.performancerRating = 5 // coba uncomment. pasti merah (error)
+
+    //3. test computed property
+    println("pajak yang harus dibayar : ${e.tax}")
     println("--- Test Error ---")
     e.salary = 5000 // ini akan meledakkan  program anda
 }
