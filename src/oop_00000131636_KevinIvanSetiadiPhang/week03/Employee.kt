31c9c0dcc80ac7b119ed99537f464c2027959b45
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-20 15:39:57 +0700

    week03: add private property

--- src/oop_00000131636_KevinIvanSetiadiPhang/week03/Employee.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week03/Employee.kt
@@ -10,4 +10,18 @@ class Employee(val name: String) {
             field = value // digunakan field untuk assign nilai asli
         }
     }
+
+    //Hanya bisa diakases di file/class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasPerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating :$performanceRating")
+    }
+
+    //kita tidak buat getter untuk performanceRating. jadi data ini benar benar rahasia
+    // kecuali kita buat function khusus untuk print
+    fun printStatus(){
+        println("Karyawan: $name, Rating : $performanceRating")
+    }
 }
