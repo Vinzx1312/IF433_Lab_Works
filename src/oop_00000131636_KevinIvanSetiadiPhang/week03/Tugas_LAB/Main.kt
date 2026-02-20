345a1034a00000f9b4230bc4c8cc9fcda3552943
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-02-20 16:58:33 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000131636_KevinIvanSetiadiPhang/week03/Tugas_LAB/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week03/Tugas_LAB/Main.kt
@@ -2,10 +2,21 @@ package oop_00000131636_KevinIvanSetiadiPhang.week03.Tugas_LAB
 
 fun main() {
 
-    val weapon = Weapon("Excalibur")
+    // ===== Testing Weapon =====
+    val weapon = Weapon("Sawiter")
 
-    weapon.damage = -50     // harus gagal
-    weapon.damage = 9999    // harus jadi 1000
+    weapon.damage = -50
+    weapon.damage = 9999
 
     println("Tier weapon: ${weapon.tier}")
+
+    println("========================")
+
+    // ===== Testing Player =====
+    val player = Player("Bahlil")
+
+    // player.xp  // ini harus error (karena private)
+
+    player.addXp(50)   // masih level 1
+    player.addXp(60)   // total 110 → level 2
 }
--- src/oop_00000131636_KevinIvanSetiadiPhang/week03/Tugas_LAB/Weapon.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week03/Tugas_LAB/Weapon.kt
@@ -3,24 +3,24 @@ package oop_00000131636_KevinIvanSetiadiPhang.week03.Tugas_LAB
 class Weapon(val name: String) {
 
     var damage: Int = 0
-        set(value) {
-            if (value < 0) {
-                println("Damage tidak boleh negatif bang!")
-                return
-            }
-
-            field = if (value > 1000) {
-                println("Damage terlalu besar! (fix cheat) Di-set menjadi 1000.")
-                1000
-            } else {
-                value
-            }
+    set(value) {
+        if (value < 0) {
+            println("Damage tidak boleh negatif bang!")
+            return
         }
+
+        field = if (value > 1000) {
+            println("Damage terlalu besar! (fix cheat) Di-set menjadi 1000.")
+            1000
+        } else {
+            value
+        }
+    }
 
     val tier: String
-        get() = when {
-            damage > 800 -> "Legendary"
-            damage > 500 -> "Epic"
-            else -> "Common"
-        }
+    get() = when {
+        damage > 800 -> "Legendary"
+        damage > 500 -> "Epic"
+        else -> "Common"
+    }
 }
