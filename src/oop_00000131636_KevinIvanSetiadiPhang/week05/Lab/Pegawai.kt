2b4c763239f406f68b80745647cd0bd500307515
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-03 19:04:13 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week05/Lab/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week05.Lab
+
+abstract class Pegawai(val nama: String) {
+    // perccakapan abstract tidak memiliki bosy (tanpa kurung kerawal)
+    abstract fun bekerja()
+}
