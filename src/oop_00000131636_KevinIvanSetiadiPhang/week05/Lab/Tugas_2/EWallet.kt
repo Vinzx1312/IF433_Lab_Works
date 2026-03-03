4ef7e2336d5151c82ac040633a1404afb216c080
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-03 21:13:39 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week05/Lab/Tugas_2/EWallet.kt
@@ -0,0 +1,22 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week05.Lab.Tugas_2
+
+class EWallet(
+    accountName: String,
+    var balance: Double
+) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil menggunakan EWallet.")
+            println("Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo sekarang: $balance")
+    }
+}
