3cf7d227f5fefcdf9d97e855430b61851dfa14f3
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-10 16:08:07 +0700

    week06: create Gopay and CreditCard

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week06/Lab/PaymentImplementations.kt
@@ -0,0 +1,11 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week06.Lab
+
+class gopay : PaymentMethod{
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")}
+    }
+
+    class CreditCard : PaymentMethod{
+        override fun pay(amount: Double) {
+            println("Cantacting Bank for Rp$amount")}
+        }
