ae3667f372f7e1265ea180c7fc1de2ba0452bc99
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-10 16:36:04 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week06/Tugas/Smartspeaker.kt
@@ -0,0 +1,19 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week06.Tugas
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("[$name] Speaker aktif dan siap menerima perintah suara.")
+    }
+
+    override fun turnOff() {
+        println("[$name] Speaker dimatikan. Sampai jumpa!")
+    }
+
+    fun playMusic(song: String) {
+        println("[$name] Memutar lagu $song dari Spotify.")
+    }
+}
