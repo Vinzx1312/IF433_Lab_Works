bd33eeb24dd255fc96d6936d967a3908f88e87a8
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-10 15:19:14 +0700

    week06: create Button class implementing Clickable

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week06/Lab/Button.kt
@@ -0,0 +1,9 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week06.Lab
+
+import oop_00000131636_KevinIvanSetiadiPhang.week06.latihan.Mahasiswa
+
+class Button (override val name: String) :  {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
+}
---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week06/Lab/Watch.kt
@@ -0,0 +1,6 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week06.Lab
+
+//abstract class = is-a (core identity)
+abstract class Watch {
+    abstract fun showTime()
+}
