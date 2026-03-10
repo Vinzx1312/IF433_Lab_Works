92e9e8110aebfd49ecc91416cfe648ea8e907d0e
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-10 15:30:48 +0700

    week06: create compound interfaces

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week06/Lab/Capabilities.kt
@@ -0,0 +1,9 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week06.Lab
+
+//interface =can-do (behavior)
+interface BluetoothConnectable {
+    fun connetToBluetooth()
+}
+interface Rechargeable {
+    fun chargeBattery()
+}
