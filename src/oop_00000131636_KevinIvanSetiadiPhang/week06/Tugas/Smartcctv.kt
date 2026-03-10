028b0d80fe1173ed2e50f8e3c1792d0bd5f6478a
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-10 16:37:01 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week06/Tugas/Smartcctv.kt
@@ -0,0 +1,21 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week06.Tugas
+
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("[$name] Kamera CCTV menyala.")
+        startRecord() // Otomatis mulai rekam saat dinyalakan
+    }
+
+    override fun turnOff() {
+        println("[$name] Kamera CCTV dimatikan.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("[$name] Mulai merekam video beresolusi 1080p...")
+    }
+}
