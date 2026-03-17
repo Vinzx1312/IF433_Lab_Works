8172634afac03c768e8764138f77456a78b39830
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-17 16:15:11 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/SystemStates.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/SystemStates.kt
@@ -2,4 +2,10 @@ package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB
 
 enum class AppState {
     STARTING, RUNNING, STOPPED
+
+    sealed class ApiResponse {
+        data class Success(val data: String) : ApiResponse()
+        data class Error(val error: String) : ApiResponse()
+        object Loading : ApiResponse()
+    }
 }
