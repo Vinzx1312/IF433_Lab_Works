e8f759374afaa010184cde3063812174c8871563
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-17 16:25:37 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/SystemStates.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/SystemStates.kt
@@ -1,11 +1,10 @@
 package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB
 
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
 enum class AppState {
     STARTING, RUNNING, STOPPED
-
-    sealed class ApiResponse {
-        data class Success(val data: String) : ApiResponse()
-        data class Error(val error: String) : ApiResponse()
-        object Loading : ApiResponse()
-    }
 }
