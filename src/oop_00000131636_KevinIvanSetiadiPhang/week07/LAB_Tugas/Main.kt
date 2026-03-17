a5f4bee35bf8032ebf9f47f8c97b122f303c28fe
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-17 16:54:50 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB_Tugas/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB_Tugas/Main.kt
@@ -3,4 +3,11 @@ package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB_Tugas
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println("Drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata awal: ${starterWeapon.item.name}, Damage: ${starterWeapon.item.damage}")
+
+
 }
