5a636659b86e8e07ceec6581fd501ddbadadcc46
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-17 16:49:39 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB_Tugas/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB_Tugas
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(20),
+    EPIC(5),
+    LEGENDARY(1)
+}
