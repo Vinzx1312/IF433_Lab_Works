069546d58b74f4718d61ce9f8d5146d1548970a6
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-17 16:01:13 +0700

    week07: implement data class for structural equality

--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/Main.kt
@@ -14,4 +14,11 @@ fun main() {
     val reg2 = RegularUser("Alice", 22)
     println(reg1) // akan mencetka memori hash
     println("Sama? ${reg1 == reg2}") // false
+
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}") //true (structural equality)
+
 }
--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/UserModels.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/UserModels.kt
@@ -2,3 +2,6 @@ package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB
 
 class RegularUser(val name: String,
     val age: Int);
+
+data class DataUser(val name: String,
+    val age: Int)
