9b069297fefc5c1088c45d8ebf6e72fbd2ccc440
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-03-17 15:44:54 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/Main.kt
@@ -1,3 +1,11 @@
 package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB
 
-val cLient = NetworkCLient("http://api.umn.ac.id")
+fun main() {
+    println("==== TEST SINGLETON ===")
+    println("Status ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n === TEST COMPANION OBJECT ===")
+    val cLient = NetworkClient.createClient() //instansiasi lewat factory
+    cLient.connect()
+}
--- src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/NetworkClient.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week07/LAB/NetworkClient.kt
@@ -1,12 +1,12 @@
 package oop_00000131636_KevinIvanSetiadiPhang.week07.LAB
 
-class NetworkCLient private constructor(val url: String) {
+class NetworkClient private constructor(val url: String) {
     companion object {
         const val BASE_URL = "https://api.umn.ac.id" // shared constant
 
-        fun createClient(): NetworkCLient {
+        fun createClient(): NetworkClient {
             println("Membangun NetworkClient dengan Base-URL: $BASE_URL")
-            return NetworkCLient(BASE_URL)
+            return NetworkClient(BASE_URL)
         }
     }
     fun connect() {
