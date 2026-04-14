cfed170bd9fffda8e15c85b6a0ed43da020938de
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-04-14 16:02:48 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week08/LatihanLab/LegacyJavaAPI.java
@@ -0,0 +1,9 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week08.LatihanLab;
+
+public class LegacyJavaAPI {
+    //fungsi ini ditulis dala java. kotlin tidak tahu apakah kembaliannya nulliable
+
+    public static String fetchServerStatus(){
+        return "SERVER_ONLINE_200_OK";
+    }
+}
