157ff8eaaa0cdbd841503bdc45be4eec917599ab
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-04-14 15:43:59 +0700

    week08: create mixed data collection

--- src/oop_00000131636_KevinIvanSetiadiPhang/week08/LatihanLab/Main.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week08/LatihanLab/Main.kt
@@ -28,4 +28,18 @@ fun main() {
         "Laptop",
         4500000.0
     )
+
+    for(item in mixedData) {
+        val text = item as? String
+
+        //Hanya cetak jika cast sukses (text tidak null)
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
+
+    val someObject: Any = 100 //tipe aslinya insteger
+    //coba cast ke String. jika gagal (null), ganti dengan "unknown string"
+    val safeString = someObject as? String?: "unknown String"
+    println("Hasil cast + fallback: safeString")
 }
