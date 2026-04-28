300e3899f4311889a7a001a6a0f5650ddeb43642
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-04-28 16:50:55 +0700

    week10: (task) populate coin repository

--- src/oop_00000131636_KevinIvanSetiadiPhang/week10/Lab/CryptoDashboard.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week10/Lab/CryptoDashboard.kt
@@ -4,3 +4,7 @@ fun main() {
     // Checkpoint 15: Initialize coin repository
     val coinRepo = WalletRepository<Coin>()
 
+    // Checkpoint 16: Populate coin repository dengan minimal 3 koin
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.75))
+    coinRepo.add(Coin("USDT", 500.0))
