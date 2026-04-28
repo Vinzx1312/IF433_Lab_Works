de9165b30a931a93d8ff9209f063dea820448f1f
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-04-28 16:03:59 +0700

    week10: implement generic function with return type

--- src/oop_00000131636_KevinIvanSetiadiPhang/week10/Lab/GenericMethods.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week10/Lab/GenericMethods.kt
@@ -3,3 +3,7 @@ package oop_00000131636_KevinIvanSetiadiPhang.week10.Lab
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 }
+
+fun <T> processData(input: T): T {
+    return input
+}
