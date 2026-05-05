60ec0f1959de9cf8184cda4c095e3f6f73ee78a5
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-05 15:49:19 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000131636_KevinIvanSetiadiPhang/week11/tugas/SmartHomeDashboard.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week11/tugas/SmartHomeDashboard.kt
@@ -26,3 +26,13 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+
+
+    // CHECKPOINT 16: Konfigurasi AC & Kabel (run)
+    val acUnit = run {
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(acUnit)
+
+    // Tambahkan alat pakan peliharaan
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
