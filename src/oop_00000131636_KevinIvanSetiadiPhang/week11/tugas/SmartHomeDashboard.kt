0e752edf037ced89a6783857ce985343d506832d
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-05 15:48:19 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000131636_KevinIvanSetiadiPhang/week11/tugas/SmartHomeDashboard.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week11/tugas/SmartHomeDashboard.kt
@@ -8,3 +8,12 @@ fun main() {
 
     // CHECKPOINT 13: Initialize smart home device list
     val homeDevices = mutableListOf<SmartDevice>()
+
+    //check 14
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+        println("[SETUP] Perangkat pencahayaan ditambahkan: ${it.name}")
+    }
