77139dc5f9360681237dc5a0d660260cd72d709e
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-05 15:27:57 +0700

    week11: test extension functions in main

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week11/Lab/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week11.Lab
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
