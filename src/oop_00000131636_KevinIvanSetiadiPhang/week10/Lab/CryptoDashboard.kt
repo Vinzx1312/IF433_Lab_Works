5861087eaf75e05425ec753840566cc470ed4240
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-05 15:25:31 +0700

    week11: create string extension functions

--- src/oop_00000131636_KevinIvanSetiadiPhang/week10/Lab/CryptoDashboard.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week10/Lab/CryptoDashboard.kt
@@ -1,6 +1,6 @@
 package oop_00000131636_KevinIvanSetiadiPhang.week10.Lab
 
-fun main() {
+fun runDashboard() {
     // Checkpoint 15: Initialize coin repository
     val coinRepo = WalletRepository<Coin>()
 
@@ -8,7 +8,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 2.75))
     coinRepo.add(Coin("USDT", 500.0))
-
 
     // Checkpoint 17: Wrap repository data into ApiResponse
     val response = ApiResponse("200 OK", coinRepo.getAll())
@@ -31,18 +30,6 @@ fun main() {
     txRepo.getAll().forEach { tx ->
         println("  - ID: ${tx.id}, Jumlah: ${tx.amount}")
     }
-
-    // Checkpoint 19: Initialize and populate transaction repository
-    val txRepo = WalletRepository<Transaction>()
-    txRepo.add(Transaction("TX001", 0.1))
-    txRepo.add(Transaction("TX002", 1.5))
-    txRepo.add(Transaction("TX003", 250.0))
-
-    println("\n=== RIWAYAT TRANSAKSI ===")
-    txRepo.getAll().forEach { tx ->
-        println("  - ID: ${tx.id}, Jumlah: ${tx.amount}")
-    }
-
 
     // Checkpoint 20: Final test - cari koin dengan balance > 1.0
     println("\n=== PENCARIAN: Koin dengan balance > 1.0 ===")
