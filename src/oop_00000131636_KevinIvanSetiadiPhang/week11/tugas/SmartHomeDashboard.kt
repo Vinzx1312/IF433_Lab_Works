4202bbef772cff3b1ffe8d19b368c0a2468554a8
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-05 15:47:41 +0700

    week11: (task) initialize smart home device list

---
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week11/tugas/SmartHomeDashboard.kt
@@ -0,0 +1,10 @@
+package oop_00000131636_KevinIvanSetiadiPhang.week11.tugas
+
+
+fun main() {
+    println("========================================")
+    println("    SMART HOME CONFIGURATION PIPELINE   ")
+    println("========================================")
+
+    // CHECKPOINT 13: Initialize smart home device list
+    val homeDevices = mutableListOf<SmartDevice>()
