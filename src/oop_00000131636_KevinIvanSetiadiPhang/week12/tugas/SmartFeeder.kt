b5122716c7073f8b2bff28605b97e808179300cf
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-12 15:35:43 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000131636_KevinIvanSetiadiPhang/week12/tugas/SmartFeeder.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week12/tugas/SmartFeeder.kt
@@ -4,10 +4,12 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
     if (isJammed) {
-        throw DispenserJamException()
+        throw DispenserJamException()//
     }
 
     if (requestedGram > availableGram) {
         throw FoodEmptyException(requestedGram, availableGram)//
     }
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
