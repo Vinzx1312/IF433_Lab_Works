d0131f6d52dcaf21c02822e4a59374a7f9622b9c
Kevin Ivan Setiadi Phang <phangkevin06@gmail.com>
2026-05-12 15:44:18 +0700

    week12: (task) handle feeding failure using onFailure

--- src/oop_00000131636_KevinIvanSetiadiPhang/week12/tugas/SmartFeeder.kt
+++ src/oop_00000131636_KevinIvanSetiadiPhang/week12/tugas/SmartFeeder.kt
@@ -42,8 +42,11 @@ fun main() {
             isJammed = false
         )
     }.onSuccess { newStock ->
-    currentKibbleStock = newStock
-    println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
-}
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+    }.onFailure { error ->
+        println("Peringatan ke Pemilik: ${error.message}")
+        println("(Opsional: Berikan chicken jerky secara manual)")
+    }
 
 }
